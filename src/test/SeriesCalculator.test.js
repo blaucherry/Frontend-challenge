bacb794383517a5c48f97b162321165b17b21460
Vianey Alcantar <angi.alcantar@hotmail.com>
2025-11-12 10:42:51 -0600

    Test

---
+++ src/test/SeriesCalculator.test.js
@@ -0,0 +1,27 @@
+// src/test/SeriesCalculator.test.js
+
+import SeriesCalculator from '../core/series/SeriesCalculator'
+
+describe('SeriesCalculator', () => {
+  const calculator = new SeriesCalculator()
+
+  const testCases = [
+    { n: 0, expected: 3 },
+    { n: 1, expected: 2 },
+    { n: 2, expected: 1 },
+    { n: 3, expected: -2 },
+    { n: 4, expected: -4 },
+    { n: 5, expected: -9 },
+    { n: 6, expected: -12 },
+    { n: 7, expected: -16 },
+    { n: 8, expected: -15 },
+    { n: 9, expected: -6 }
+  ]
+
+  testCases.forEach(({ n, expected }) => {
+    test(`serie(${n}) should return ${expected}`, () => {
+      const result = calculator.calculate(n)
+      expect(result).toBe(expected)
+    })
+  })
+})
